8bc6e321d907e72878cf2adfc5035bece83b5a59
VladParfan <vladparfan1997@gmail.com>
2023-02-05 20:35:20 +0100

    Custom Login Page

--- HappyNews/src/main/java/com/fdmgroup/HappyNews/config/SecurityConfig.java
+++ HappyNews/src/main/java/com/fdmgroup/HappyNews/config/SecurityConfig.java
@@ -1,17 +1,22 @@
 package com.fdmgroup.HappyNews.config;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.fdmgroup.HappyNews.security.AuthProviderImpl;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebMvcConfigurerAdapter{
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	
 	private final AuthProviderImpl authProvider;
@@ -22,8 +27,23 @@ public class SecurityConfig extends WebMvcConfigurerAdapter{
 		this.authProvider = authProvider;
 	}
 
-
-	protected void configure(AuthenticationManagerBuilder auth) {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception
+		{
+			
+		http.csrf().disable()
+		.authorizeRequests()
+			.antMatchers("/auth/login","/error").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().loginPage("/auth/login")
+			.loginProcessingUrl("/process_login")
+			.defaultSuccessUrl("/hello", true)
+			.failureUrl("/auth/login?error");
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.authenticationProvider(authProvider);
 	}
 	
