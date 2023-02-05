85aeed7cb6de6647cd312a1c3a837295edc5bf5b
VladParfan <vladparfan1997@gmail.com>
2023-02-05 12:08:11 +0100

    Add existing project to files to Git

---
+++ HappyNews/src/main/java/com/fdmgroup/HappyNews/HappyNewsApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.HappyNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyNewsApplication.class, args);
+	}
+
+}
