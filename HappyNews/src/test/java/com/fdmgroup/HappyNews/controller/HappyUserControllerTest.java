b69a51c9a9c4699ebe87ccc537d9a2713c32e0c8
weronikakonior <124500369+weronikakonior@users.noreply.github.com>
2023-02-10 20:09:39 +0100

    Update HappyUserControllerTest.java

--- HappyNews/src/test/java/com/fdmgroup/HappyNews/controller/HappyUserControllerTest.java
+++ HappyNews/src/test/java/com/fdmgroup/HappyNews/controller/HappyUserControllerTest.java
@@ -1,5 +1,29 @@
 package com.fdmgroup.HappyNews.controller;
 
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fdmgroup.HappyNews.model.HappyUser;
+import com.fdmgroup.HappyNews.repository.HappyUserRepository;
+import com.fdmgroup.HappyNews.service.HappyUserDetailsService;
+
 public class HappyUserControllerTest {
 
+	@MockBean	
+	MainController mockMainController;
+
+	@MockBean
+	HappyUserDetailsService mockhappyUserService;
+	
+	@MockBean	
+	
+	@Mock
+	HappyUser happyUser;
+	private HappyUserRepository userRepository;
+	@Autowired
+	private MockMvc mockMvc;
+	
+	
 }
